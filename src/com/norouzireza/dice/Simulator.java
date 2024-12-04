0db1a5ef61313d4848d0c2c3dd796c14002113d7
newsa720 <rda0044@gmail.com>
2024-12-04 22:36:07 +0330

    initialize repo

---
+++ src/com/norouzireza/dice/Simulator.java
@@ -0,0 +1,11 @@
+package com.norouzireza.dice;
+
+import java.util.Random;
+
+public class Simulator {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        var x = rand.nextInt(1,6);
+
+    }
+}
